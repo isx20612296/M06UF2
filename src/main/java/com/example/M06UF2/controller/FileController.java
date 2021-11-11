2c9bc33a17728d5aa515494f7a20f60da09d7135
andreupasalamar <andreupas23@gmail.com>
2021-11-11 19:52:25 +0100

    Arreglos file

--- src/main/java/com/example/M06UF2/controller/FileController.java
+++ src/main/java/com/example/M06UF2/controller/FileController.java
@@ -2,32 +2,26 @@ package com.example.M06UF2.controller;
 
 import com.example.M06UF2.domain.File;
 import com.example.M06UF2.repository.FileRepository;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
 import java.util.UUID;
-
 
 @RestController
 @RequestMapping("/files")
 public class FileController {
 
-    FileRepository fileRepository;
-
-    FileController(com.example.M06UF2.repository.MovieRepository movieRepository) {
+    private final FileRepository fileRepository;
+    FileController(FileRepository fileRepository){
         this.fileRepository = fileRepository;
-    }
-
-    @GetMapping("/{id}")
-    public byte[] jajajajajajaj(PathVariable UUID id) {
-        File file = fileRepository.getById(id);
-        return file.data;
     }
 
     @PostMapping
     public String upload(@RequestParam("file") MultipartFile uploadedFile) {
         try {
-            System.out.println(uploadedFile.getOriginalFilename() + ", " + uploadedFile.getContentType());
             File file = new File();
             file.contenttype = uploadedFile.getContentType();
             file.data = uploadedFile.getBytes();
@@ -35,11 +29,20 @@ public class FileController {
             return fileRepository.save(file).fileid.toString();
         } catch (Exception e) {
             e.printStackTrace();
+            return null;
         }
-        return null;
     }
-//    @GetMapping("/")
-//    public List<File> talicual() {
-//        return fileRepository.findAll();
-//    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<byte[]> getFile(@PathVariable UUID id) {
+        File file = fileRepository.findById(id).orElse(null);
+
+        if (file == null) return ResponseEntity.notFound().build();
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.valueOf(file.contenttype))
+                .contentLength(file.data.length)
+                .body(file.data);
+    }
 }
